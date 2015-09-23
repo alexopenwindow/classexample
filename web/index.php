16122363e7c3c33cfa044b7671d15b30e325e6ad
Alexander Skelton <ajrskelton@gmail.com>
2015-09-23 11:28:13 +0200

    Display database info

--- web/index.php
+++ web/index.php
@@ -1,11 +1,27 @@
+<?php
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    $conn = new mysqli($server, $username, $password, $db);
+    
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Your name goes here</title>
+        <title>ajrskelton</title>
     </head>
     <body>
         <?php
-            echo "<h1>Hello World</h1>";
+            echo "<h1>Database info:</h1>";
+            echo "<p>Server: $server</p>";
+            echo "<p>Username: $username</p>";
+            echo "<p>Password: $pasword</p>";
+            echo "<p>DB: $db</p>";
         ?>
     </body>
 </html>
